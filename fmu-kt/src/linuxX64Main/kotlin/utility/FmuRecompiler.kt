32dd224e77a66f350e44c9274a6708b4432b3768
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2026-04-02 18:54:18 +0200

    refactor: split implementation for different targets for the compiler

---
+++ fmu-kt/src/linuxX64Main/kotlin/utility/FmuRecompiler.kt
@@ -0,0 +1,8 @@
+package utility
+
+actual class FmuRecompiler {
+    actual fun recompile(inputFmu: String, outputFmu: String) {
+        println("Skipping recompilation on linuxX64, as it is not supported yet.")
+    }
+
+}
