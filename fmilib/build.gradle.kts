eb8adbac8efc78762f837ed83afdd0f6b376cb6b
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2026-04-05 01:05:20 +0200

    feat(fmilib): add buiding with plugin + task to do cmake install

--- fmilib/build.gradle.kts
+++ fmilib/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.support.serviceOf
+
 plugins {
     alias(libs.plugins.gradle.cmake)
 }
@@ -14,4 +16,42 @@ cmake {
             targetMachines.add(machines.host)
         }
     }
+}
+
+
+val fmilibBuildDir = layout.buildDirectory.dir("cmake/fmilib").get().asFile
+val fmilibInstallDir = layout.buildDirectory.dir("fmilib-install").get().asFile
+
+val cmakeInstall by tasks.registering {
+    dependsOn("cmakeBuild")
+    outputs.dir(fmilibInstallDir)
+
+    val execOps = project.serviceOf<ExecOperations>()
+
+    doLast {
+        val platformDirs = fmilibBuildDir.listFiles()?.filter { it.isDirectory }
+            ?: error("cmake build dir non trovata in $fmilibBuildDir")
+
+        platformDirs.forEach { platformDir ->
+            val platformInstallDir = fmilibInstallDir.resolve(platformDir.name)
+            execOps.exec {
+                commandLine(
+                    "cmake", "--install", platformDir.absolutePath,
+                    "--prefix", platformInstallDir.absolutePath
+                )
+            }
+        }
+    }
+}
+
+tasks.named("cmakeBuild") {
+    finalizedBy(cmakeInstall)
+}
+
+val cmakeInstallClean by tasks.registering(Delete::class) {
+    delete(fmilibInstallDir)
+}
+
+tasks.named("cmakeClean") {
+    finalizedBy(cmakeInstallClean)
 }
