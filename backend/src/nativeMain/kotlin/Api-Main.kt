0fbfb037c84ad7c68126b8a0127edff14c7b3f1a
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2026-04-26 14:36:04 +0200

    fix: now working with absolute paths for better portability

--- backend/src/nativeMain/kotlin/Api-Main.kt
+++ backend/src/nativeMain/kotlin/Api-Main.kt
@@ -2,13 +2,15 @@ import configuration.configureCors
 import configuration.configureRouting
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
+import utility.FilesystemManager
 
 fun main(args: Array<String>) {
     val baseDir = if (args.isNotEmpty()) args[0] else "."
+    val absoluteBaseDir = FilesystemManager().pathAbsolute(baseDir)
 
     embeddedServer(CIO, port = 8080) {
         configureCors()
         //configureSerialization()
-        configureRouting(baseDir)
+        configureRouting(absoluteBaseDir)
     }.start(wait = true)
 }
