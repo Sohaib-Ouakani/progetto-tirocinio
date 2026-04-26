a09c214bdaff3fced5059494af01d32b349fba44
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2026-04-26 15:12:42 +0200

    fix: delete unused import of FileSystemManger that causes bug related to platform compatibility

--- backend/src/nativeMain/kotlin/Api-Main.kt
+++ backend/src/nativeMain/kotlin/Api-Main.kt
@@ -2,7 +2,6 @@ import configuration.configureCors
 import configuration.configureRouting
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
-import utility.FilesystemManager
 
 fun main(args: Array<String>) {
     val baseDir = if (args.isNotEmpty()) args[0] else "."
