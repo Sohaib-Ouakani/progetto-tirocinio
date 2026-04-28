016c37eece12cc96fa4b4f57eac34b641be10216
Danilo Pianini <danilo.pianini@unibo.it>
2026-04-28 11:37:44 +0200

    build: add kotlin-qa plugin for improved code quality checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.gradle.cmake) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlinxSerialization) apply false
+    id("org.danilopianini.gradle-kotlin-qa") version "1.6.0"
     id("com.dorongold.task-tree") version "4.0.1"
     `lifecycle-base`
 }
