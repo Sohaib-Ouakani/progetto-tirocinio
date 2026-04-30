216c783491eb7d128ddcbc115e6b0f040799a773
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2026-04-30 11:47:22 +0200

    fix: actual notation added for mingw folder

--- fmu-kt/src/mingwX64Main/kotlin/recompiler/FmuRecompiler.kt
+++ fmu-kt/src/mingwX64Main/kotlin/recompiler/FmuRecompiler.kt
@@ -1,7 +1,9 @@
 package recompiler
 
 actual class FmuRecompiler {
-    fun recompile(inputFmu: String, outputFmu: String) {
+
+    actual fun recompile(inputFmu: String, outputFmu: String) {
         println("Skipping recompilation on mingwX64, as it is not supported yet.")
     }
+
 }
