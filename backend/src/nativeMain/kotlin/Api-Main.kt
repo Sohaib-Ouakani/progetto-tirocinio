5c22a8c6d05716957cb63174b82d23f448039d59
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2026-04-30 21:48:03 +0200

    feat: add creation and cleanup of resource folder tree

--- backend/src/nativeMain/kotlin/Api-Main.kt
+++ backend/src/nativeMain/kotlin/Api-Main.kt
@@ -1,5 +1,6 @@
 import configuration.configureCors
 import configuration.configureRouting
+import io.ktor.server.application.ApplicationStopped
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 import resources.manager.ResourceManager
@@ -11,6 +12,10 @@ fun main(args: Array<String>) {
     val server = embeddedServer(CIO, port = 8080) {
         configureCors()
         configureRouting(resourceManager)
+
+        monitor.subscribe(ApplicationStopped) {
+            resourceManager.terminateResourcesDirectory()
+        }
     }
 
     server.addShutdownHook { resourceManager.terminateResourcesDirectory() }
